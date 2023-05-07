9d11587631d741ea6d70b0a83208e8929fbf430f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-06 23:59:46 -0600

    pomodoro message implemented

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@/app'
 import { pauseTimer, startTimer, tick } from '@/app/slices/pomodoro/personal'
 import PomodoroTimerUI from './PomodoroTimerUI'
+import { AppMessageContext } from '@/context/AppMessageContext'
+import appMessages from '@/constants/messages/app'
 
 export default function PomodoroTimer() {
   const { minutes, seconds, isPaused, status } = useSelector(
     (state: RootState) => state.personalPomodoro
   )
+  const { handleMessage } = useContext(AppMessageContext)
 
   const dispatch = useDispatch()
   const [anchorElMoreOptions, setAnchorElMoreOptions] =
@@ -37,10 +40,12 @@ export default function PomodoroTimer() {
 
   const handleStartTimer = () => {
     dispatch(startTimer())
+    handleMessage(appMessages.pomodoro.sessionStartMessage)
   }
 
   const handlePauseTimer = () => {
     dispatch(pauseTimer())
+    handleMessage(appMessages.pomodoro.sessionEndMessage)
   }
 
   return (
