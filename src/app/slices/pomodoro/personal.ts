561a85ad3aca300603da27d5b7c86851a4401ede
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-07 13:44:52 -0600

    Refactor pomodoroTimerSlice to improve developer experience by utilizing reducers from separate folder

--- src/app/slices/pomodoro/personal.ts
+++ src/app/slices/pomodoro/personal.ts
@@ -1,9 +1,19 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import { pomodoroTimer } from '@/utils/basic/pomodoroTimer'
+import startTimerReducer from './reducers/startTimer'
+import pauseTimerReducer from './reducers/pauseTimer'
+import resetTimerReducer from './reducers/resetTimer'
+import goToNextIntervalReducer from './reducers/goToNextInterval'
+import updateFocusTimeDurationReducer from './reducers/updateFocusTimeDuration'
+import updateShortBreakDurationReducer from './reducers/updateShortBreakDuration'
+import updateLongBreakDurationReducer from './reducers/updateLongBreakDuration'
+import updatePomodoroIntervalsReducer from './reducers/updatePomodoroIntervals'
+import resetSettingsReducer from './reducers/resetSettings'
+import tickReducer from './reducers/tick'
 
 export type PomodoroStatus = 'Pause' | 'Focus' | 'Break' | 'Long break'
 
-interface PomodoroTimerState {
+export interface PomodoroTimerState {
   // Pomodoro State
   isPaused: boolean
   status: PomodoroStatus
@@ -41,85 +51,16 @@ const pomodoroTimerSlice = createSlice({
   name: 'pomodoroTimer',
   initialState,
   reducers: {
-    startTimer(state) {
-      state.isPaused = false
-      state.status = 'Focus'
-    },
-    pauseTimer(state) {
-      state.isPaused = true
-      state.status = 'Pause'
-    },
-    resetTimer(state) {
-      state.isPaused = true
-      state.status = 'Pause'
-      state.minutes = state.userFocusTimeDuration
-      state.seconds = 0
-    },
-    goToNextInterval(state) {
-      if (state.currentInterval < state.userPomodoroIntervals) {
-        if (state.status === 'Break') {
-          // Increment the current interval by 1, ensuring it doesn't exceed the maximum number of pomodoro intervals
-          state.currentInterval = Math.min(
-            state.currentInterval + 1,
-            state.userPomodoroIntervals
-          )
-          state.minutes = state.userFocusTimeDuration
-        } else if (state.status === 'Focus') {
-          state.minutes = state.userShortBreakDuration
-        }
-
-        state.seconds = 0
-        state.status = state.status === 'Focus' ? 'Break' : 'Focus'
-      } else {
-        state.status = 'Long break'
-        state.minutes = state.userLongBreakDuration
-        state.seconds = 0
-      }
-    },
-
-    updateFocusTimeDuration(state, action: PayloadAction<number>) {
-      state.userFocusTimeDuration = action.payload
-      state.seconds = 0
-    },
-    updateShortBreakDuration(state, action: PayloadAction<number>) {
-      state.userShortBreakDuration = action.payload
-    },
-    updateLongBreakDuration(state, action: PayloadAction<number>) {
-      state.userLongBreakDuration = action.payload
-    },
-    updatePomodoroIntervals(state, action: PayloadAction<number>) {
-      state.userPomodoroIntervals = action.payload
-    },
-
-    resetSettings(state) {
-      state.isPaused = true
-      state.status = 'Pause'
-
-      state.minutes = pomodoroTimer.focusTimer.defaultDuration
-      state.seconds = 0
-
-      state.userFocusTimeDuration = pomodoroTimer.focusTimer.defaultDuration
-      state.userShortBreakDuration =
-        pomodoroTimer.shortBreakTimer.defaultDuration
-      state.userLongBreakDuration = pomodoroTimer.longBreakTimer.defaultDuration
-      state.userPomodoroIntervals = pomodoroTimer.intervalTimer.defaultDuration
-    },
-
-    tick(state) {
-      if (!state.isPaused) {
-        if (state.seconds === 0) {
-          if (state.minutes === 0) {
-            // Timer reached 0
-            // Handle logic for switching to the next interval
-          } else {
-            state.minutes -= 1
-            state.seconds = 59
-          }
-        } else {
-          state.seconds -= 1
-        }
-      }
-    },
+    startTimer: startTimerReducer,
+    pauseTimer: pauseTimerReducer,
+    resetTimer: resetTimerReducer,
+    goToNextInterval: goToNextIntervalReducer,
+    updateFocusTimeDuration: updateFocusTimeDurationReducer,
+    updateShortBreakDuration: updateShortBreakDurationReducer,
+    updateLongBreakDuration: updateLongBreakDurationReducer,
+    updatePomodoroIntervals: updatePomodoroIntervalsReducer,
+    resetSettings: resetSettingsReducer,
+    tick: tickReducer,
   },
 })
 
@@ -127,13 +68,11 @@ export const {
   startTimer,
   pauseTimer,
   resetTimer,
-
   updateFocusTimeDuration,
   updateShortBreakDuration,
   updateLongBreakDuration,
   updatePomodoroIntervals,
   goToNextInterval,
-
   tick,
 } = pomodoroTimerSlice.actions
 
