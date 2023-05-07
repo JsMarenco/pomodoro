4948beb5da0b42decb3824f014eeeac6b1e13fb0
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-07 10:55:54 -0600

    goToNextInterval logic added

--- src/app/slices/pomodoro/personal.ts
+++ src/app/slices/pomodoro/personal.ts
@@ -55,6 +55,27 @@ const pomodoroTimerSlice = createSlice({
       state.minutes = state.userFocusTimeDuration
       state.seconds = 0
     },
+    goToNextInterval(state) {
+      if (state.currentInterval < state.userPomodoroIntervals) {
+        if (state.status === 'Break') {
+          // Increment the current interval by 1, ensuring it doesn't exceed the maximum number of pomodoro intervals
+          state.currentInterval = Math.min(
+            state.currentInterval + 1,
+            state.userPomodoroIntervals
+          )
+          state.minutes = state.userFocusTimeDuration
+        } else if (state.status === 'Focus') {
+          state.minutes = state.userShortBreakDuration
+        }
+
+        state.seconds = 0
+        state.status = state.status === 'Focus' ? 'Break' : 'Focus'
+      } else {
+        state.status = 'Long break'
+        state.minutes = state.userLongBreakDuration
+        state.seconds = 0
+      }
+    },
 
     updateFocusTimeDuration(state, action: PayloadAction<number>) {
       state.minutes = action.payload
@@ -112,6 +133,7 @@ export const {
   updateShortBreakDuration,
   updateLongBreakDuration,
   updatePomodoroIntervals,
+  goToNextInterval,
 
   tick,
 } = pomodoroTimerSlice.actions
