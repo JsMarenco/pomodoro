a37499ffcf5c359c3e012a480e463b1e2e743eee
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-07 17:50:13 -0600

    Add preferences keys file

---
+++ src/constants/preferenceKeys.ts
@@ -0,0 +1,19 @@
+const preferenceKeys = {
+  focusTimer: {
+    key: 'userPreference_focusTimer',
+  },
+  shortBreakTimer: {
+    key: 'userPreference_shortBreakTimer',
+  },
+  longBreakTimer: {
+    key: 'userPreference_longBreakTimer',
+  },
+  intervalTimer: {
+    key: 'userPreference_intervalTimer',
+  },
+  theme: {
+    key: 'userPreference_theme',
+  },
+}
+
+export default preferenceKeys
