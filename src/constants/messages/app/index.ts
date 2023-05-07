9d126a255118b6b20f9a909529c5ee2d5e4346ef
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-06 23:55:17 -0600

    pomodoro messages

--- src/constants/messages/app/index.ts
+++ src/constants/messages/app/index.ts
@@ -3,12 +3,6 @@ const appMessages = {
     copyMessage:
       'The content has been copied to your clipboard. Thank you for sharing!',
   },
-  profile: {
-    avatarUpdatedMessage: 'Your avatar has been successfully updated',
-    updateMessage: 'Your profile has been successfully updated',
-    coverUpdatedMessage: 'Your cover photo has been successfully updated',
-    passwordUpdatedMessage: 'Your password has been successfully updated',
-  },
   internet: {
     lostConnectionMessage: 'You have lost your connection to the internet',
     reconnectedMessage: 'You are now reconnected to the internet',
@@ -17,19 +11,12 @@ const appMessages = {
     darkActivatedMessage: 'Dark theme activated',
     lightActivatedMessage: 'Light theme activated',
   },
-  post: {
-    editCancelledMessage: 'Your post edit has been cancelled',
-    noMorePostsMessage: 'There are no more posts. Create your own!',
-  },
-  friend: {
-    noRequestsMessage:
-      'No friend requests yet, but don\'t worry, they\'ll be coming in soon!',
-    noFriendRequestMessage: 'There is nothing here',
-    noConnectionsMessage: 'Be the first, and make new friends',
-    noFriendsMessage: 'Don\'t worry, making friends is always fun.',
-    noMoreFriendsMessage: 'There are no more friends.',
-    noPeopleHereMessage:
-      'There is no people here. Invite your friends to join!',
+  pomodoro: {
+    sessionEndMessage: 'Pomodoro session ended. Take a break!',
+    sessionStartMessage: 'Pomodoro session started. Stay focused!',
+    sessionPauseMessage: 'Pomodoro session paused',
+    shortBreakMessage: 'Take a short break. You deserve it!',
+    longBreakMessage: 'Take a long break. Recharge yourself!',
   },
 }
 
