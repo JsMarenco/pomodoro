4b6ee0479d70d5a6079a6c150cc00e32d29df85f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-08 05:11:18 -0600

    Add getValueFromLocalStorage function and make minor changes in other functions

--- src/utils/basic/index.ts
+++ src/utils/basic/index.ts
@@ -13,11 +13,29 @@ export const saveInLocalStorage = (key: string, obj: unknown) => {
  * @returns {*} - The retrieved object, or null if the key is not found.
  */
 export const getFromLocalStorage = (key: string) => {
-  const item = localStorage.getItem(key)
+  if (typeof window !== 'undefined') {
+    const item = localStorage.getItem(key)
 
-  if (item) {
-    return JSON.parse(item)
+    if (item) {
+      return JSON.parse(item)
+    }
   }
 
   return null
+}
+
+/**
+ * Retrieves a value from local storage based on the provided key.
+ * If the value is not found in local storage, returns the default value.
+ *
+ * @param {string} key - The key to look up in local storage.
+ * @param {unknown} defaultValue - The default value to return if the key is not found.
+ * @returns {unknown} The value from local storage or the default value.
+ */
+export const getValueFromLocalStorage = (
+  key: string,
+  defaultValue: unknown
+) => {
+  const storedValue = getFromLocalStorage(key)?.value
+  return storedValue || defaultValue
 }
