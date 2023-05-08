baadf23e75c43e5644fabc874d1f69dfcb66ded5
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-08 05:13:52 -0600

    Add temporary loading in pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import PomodoroTimer from '@/components/PomodoroTimer'
 import Layout from '@/components/Layout'
 import appTitles from '@/constants/titles'
 import useChangePageTitle from '@/hooks/general/useChangePageTitle'
+import AppLoader from '@/components/AppLoader'
 
 export default function Home() {
   useChangePageTitle(appTitles.homeTitle)
 
+  const [loading, setloading] = useState(true)
+
+  useEffect(() => {
+    setloading(false)
+  }, [])
+
   return (
     <Layout>
-      <PomodoroTimer />
+      {loading && <AppLoader />}
+
+      {!loading && <PomodoroTimer />}
     </Layout>
   )
 }
