d8b8fecd939404b0197bdb52a61240807f12c146
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-08 11:08:00 -0600

    Create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { AppBar, Toolbar } from '@mui/material'
+import AppLogoIcon from '../AppLogoIcon'
+
+export default function Header() {
+  return (
+    <AppBar
+      position="relative"
+      sx={{
+        bgcolor: 'primary.light'
+      }}
+    >
+      <Toolbar>
+        <AppLogoIcon />
+      </Toolbar>
+    </AppBar>
+  )
+}
