0a6adee44bdea22db7356efc2c4673961ebf7301
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-11 10:43:11 -0600

    Add styles for the menu

---
+++ src/components/Menus/styles.ts
@@ -0,0 +1,14 @@
+const wrapper = {
+  '.MuiDialog-paper': {
+    borderRadius: 2,
+    bgcolor: 'background.paper',
+    backgroundImage: 'none',
+    width: '100%',
+  },
+}
+
+const menuStyles = {
+  wrapper,
+}
+
+export default menuStyles
