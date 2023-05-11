0d71b220009ce72718beed61eab6032c78ae5114
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-11 09:42:33 -0600

    Add new messages for rooms

--- src/constants/messages/api/errors.ts
+++ src/constants/messages/api/errors.ts
@@ -1,3 +1,8 @@
-const apiErrorMessages = {}
+const apiErrorMessages = {
+  room: {
+    notCreated: 'Room creation failed.',
+    aliasInUse: 'The alias link is already in use by another user.',
+  },
+}
 
 export default apiErrorMessages
--- src/constants/messages/api/success.ts
+++ src/constants/messages/api/success.ts
@@ -1,3 +1,10 @@
-const apiSuccessMessages = {}
+const apiSuccessMessages = {
+  room: {
+    created: 'Room created successfully.',
+    updated: 'Room updated successfully.',
+    newUserEntered: 'A new user has entered the room.',
+    userExited: 'A user has exited the room.',
+  },
+}
 
 export default apiSuccessMessages
