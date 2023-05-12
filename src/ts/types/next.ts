9cfc76aad25b4de8a5254dcf454729d4867785e0
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-12 12:52:48 -0600

    Modify and add new types in types/next.ts

--- src/ts/types/next.ts
+++ src/ts/types/next.ts
@@ -1,11 +1,34 @@
-import { Server as NetServer, Socket } from 'net'
-import { NextApiResponse } from 'next'
-import { Server as SocketIOServer } from 'socket.io'
+import type { Server as HTTPServer } from 'http'
+import type { NextApiResponse } from 'next'
+import type { Socket as NetSocket } from 'net'
+import type { Server as IOServer } from 'socket.io'
 
-export type NextApiResponseServerIO = NextApiResponse & {
-  socket: Socket & {
-    server: NetServer & {
-      io: SocketIOServer
-    }
-  }
+export interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined
+}
+
+export interface SocketWithIO extends NetSocket {
+  server: SocketServer
+}
+
+export interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO
+}
+
+export interface ServerToClientEvents {
+  userServerConnection: () => void
+  sendNotif: (msg: string) => void
+  sendMsg: (msg: string, id: string) => void
+  userServerDisconnection: (socketid: string) => void
+  joinRoom: (roomid: string) => void
+  leaveRoom: (roomid: string) => void
+}
+
+export interface ClientToServerEvents {
+  sendNotif: (msg: string) => void
+  sendMsg: (msg: string, id: string) => void
+  userServerConnection: () => void
+  userServerDisconnection: (socketid: string) => void
+  joinRoom: (roomid: string) => void
+  leaveRoom: (roomid: string) => void
 }
