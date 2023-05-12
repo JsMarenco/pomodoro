50be80ba577a32ac0bae149e6998b40aa1d725d5
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-12 16:45:28 -0600

    Add a hook to play sounds

---
+++ src/hooks/general/useAudioPlayer.tsx
@@ -0,0 +1,21 @@
+import { RootState } from '@/app'
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const useAudioPlayer = () => {
+  const { soundsEnabled } = useSelector((state: RootState) => state.personalPomodoro)
+
+  const play = (soundUrl: string, volume: number) => {
+    if (soundsEnabled && soundUrl) {
+      const audio = new Audio(soundUrl)
+      audio.volume - volume
+      audio.play()
+    }
+  }
+
+  return {
+    play
+  }
+}
+
+export default useAudioPlayer
