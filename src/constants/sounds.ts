ddfd2f8025809eab2278a2632051e97d6c1f52e0
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-12 16:06:59 -0600

    Add sound URLs to appSounds

--- src/constants/sounds.ts
+++ src/constants/sounds.ts
@@ -1,6 +1,36 @@
 const appSounds = {
-  pomodoroChime: {
-    url: '/sounds/pomodoro_chime.mp3',
+  pomodoro: {
+    start: {
+      url: '/sounds/pomodoro_start.mp3',
+      volume: 0.1,
+    },
+    end: {
+      url: '/sounds/pomodoro_end.mp3',
+      volume: 0.1,
+    },
+  },
+  break: {
+    start: {
+      url: '/sounds/break_start.mp3',
+      volume: 0.1,
+    },
+    end: {
+      url: '/sounds/break_end.mp3',
+      volume: 0.1,
+    },
+  },
+  longBreak: {
+    start: {
+      url: '/sounds/long_break_start.mp3',
+      volume: 0.1,
+    },
+    end: {
+      url: '/sounds/long_break_end.mp3',
+      volume: 0.1,
+    },
+  },
+  notification: {
+    url: '/sounds/notification.mp3',
     volume: 0.1,
   },
 }
