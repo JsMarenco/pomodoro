fa6b3677f389373cd5d3a38bdc1f2c4a606a0550
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-12 20:54:50 -0600

    Small changes in PomodoroTimerStatus

--- src/components/PomodoroTimer/PomodoroTimerStatus.tsx
+++ src/components/PomodoroTimer/PomodoroTimerStatus.tsx
@@ -44,7 +44,7 @@ export default function PomodoroTimerStatus({
       )}
 
       <Typography variant="body1" color="text.primary">
-        {status}
+        {isPaused ? 'Pause' : status}
       </Typography>
     </Stack>
   )
